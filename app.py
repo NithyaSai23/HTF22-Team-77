b48075bc56fd06dc0101915265799d4c4e8a1f69
NithyaSai23 <75198977+NithyaSai23@users.noreply.github.com>
2022-10-30 11:04:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,90 @@
+from flask import Flask,render_template,Response
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+import numpy as np
+import math
+import time
+
+app=Flask(__name__, template_folder='templates')
+cap=cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+        success,frame=cap.read()
+        if not success:
+            break
+        else:
+            detector = HandDetector(maxHands=1)
+            classifier = Classifier("Model/keras_model.h5","Model/labels.txt")
+
+            offset = 20
+            imgSize = 300
+
+            folder = "images/C"
+            counter = 0
+
+            labels = ["A","B","C"]
+               
+
+            while True:
+                success, img = cap.read()
+                img = np.array(img)
+                imgOutput = img.copy()
+                hands, img = detector.findHands(img)
+                if hands:
+                    hand = hands[0]
+                    x, y, w, h = hand['bbox']
+                    imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+                    imgCrop = img[y - offset :y + h + offset, x - offset :x + w + offset]
+                    imgCropShape = imgCrop.shape
+                    aspectRatio = h/w
+
+                    if aspectRatio > 1:
+                        k = imgSize/h
+                        wCal = math.ceil(k*w)
+                        imgResize = cv2.resize(imgCrop,(wCal, imgSize))
+                        imgResizeShape = imgResize.shape
+                        wGap = math.ceil((imgSize-wCal)/2)
+                        imgWhite[:, wGap:wCal+wGap] = imgResize
+                        prediction, index = classifier.getPrediction(imgWhite, draw = False)
+                        print(prediction, index)
+                        
+                    else:
+                        k = imgSize/w
+                        hCal = math.ceil(k*h)
+                        imgResize = cv2.resize(imgCrop,(imgSize, hCal))
+                        imgResizeShape = imgResize.shape
+                        hGap = math.ceil((imgSize-hCal)/2)
+                        imgWhite[hGap:hCal+hGap, :] = imgResize
+                        prediction, index = classifier.getPrediction(imgWhite, draw = False)
+
+                    cv2.rectangle(imgOutput, (x - offset, y - offset-50),
+                                (x - offset+90, y - offset-50+50), (255, 0, 255), cv2.FILLED)
+                    cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+                    cv2.rectangle(imgOutput, (x-offset, y-offset),
+                                (x + w+offset, y + h+offset), (255, 0, 255), 4)
+
+                    #cv2.imshow("ImageCrop", imgCrop)
+                    #cv2.imshow("ImageWhite", imgWhite)
+
+                cv2.imshow("Image", imgOutput)
+                cv2.waitKey(1)
+
+                ret,buffer=cv2.imencode('.jpg',frame)
+                frame=buffer.tobytes()
+
+                yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+    app.run(debug=True)
